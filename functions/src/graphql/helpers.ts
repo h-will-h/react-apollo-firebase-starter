ff0995897248c3a2bcd8ef40b1c06f76c487ab22
Will Howard <will.howard+github@gmail.com>
2019-06-22 14:42:27 -0400

    Clean up some stray console.logs

--- functions/src/graphql/helpers.ts
+++ functions/src/graphql/helpers.ts
@@ -1,22 +1,16 @@
 import * as admin from 'firebase-admin';
 
 export const getLoggedInUser = async (loggedInUserId: string) => {
-	console.info('hiooooo!!');
-	console.info(loggedInUserId);
 	if (!loggedInUserId) {
 		return { id: null };
 	}
 	const ref = admin.database().ref(`members/${loggedInUserId}`);
 	const data = await ref.once('value').then((snapshot: any) => snapshot.val());
 	data.id = loggedInUserId;
-	console.info(data);
 	return data;
 };
 
 export const createUser = async (name: string, loggedInUserId: string) => {
-	console.info('heyooooo!!!');
-	console.info(name);
-	console.info(loggedInUserId);
 	if (!loggedInUserId) {
 		return { uid: null };
 	}
